8fb5ef691eedfb6d077aea840deffd318aad3ac2
Sarah Allerheiligen <sarahallerheiligen@Sarahs-MBP-9.quinnipiac.edu>
2025-02-12 11:49:51 -0500

    switched app.js to routing

--- bobcat-tracks/src/App.js
+++ bobcat-tracks/src/App.js
@@ -3,12 +3,21 @@ import LoginPage from "./components/login-page";
 import NavBar from "./components/navBar";
 import AttendancePage from "./components/AttendancePage";
 import UploadPage from "./components/UploadPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <UploadPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/attendance" element={<AttendancePage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <AttendancePage />
+    // </div>
   );
 }
 
