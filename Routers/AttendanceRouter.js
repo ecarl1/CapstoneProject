16576840651e2ac200400664d72ee40271cb58d5
ecarl1 <113143036+ecarl1@users.noreply.github.com>
2025-03-18 16:03:30 -0400

    push

--- Routers/AttendanceRouter.js
+++ Routers/AttendanceRouter.js
@@ -5,7 +5,7 @@ const Session = require('../SessionModels/Session');
 
 router.get('/', async (req, res) => {
     try {
-        const data = await Session.findAll({attributes: ['date', 'course_id']}); // Fetch all data from the "session" tabl
+        const data = await Session.findAll({attributes: ['date', 'course_name']}); // Fetch all data from the "session" tabl
         res.json(data); // Send the data as a JSON response
       } catch (err) {
         res.status(500).json({ error: 'Failed to retrieve session data' });
