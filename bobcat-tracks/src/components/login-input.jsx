517be733b37955c383d3b973fcac97a9f3fc7e9f
J4CKr4bb1t <sarah.allerheiligen@quinnipiac.edu>
2025-04-29 20:09:29 -0400

    fixed login styling

--- bobcat-tracks/src/components/login-input.jsx
+++ bobcat-tracks/src/components/login-input.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const url = "http://localhost:3000/api/user/login";
-
 
 const LoginForm = () => {
   const {
@@ -16,36 +15,37 @@ const LoginForm = () => {
   const navigate = useNavigate();
   const onSubmit = async (data) => {
     try {
-        console.log("Sending login request:", data);
+      console.log("Sending login request:", data);
 
-        const response = await axios.post(url, {
-            username: data.username,
-            password: data.password,
-        });
+      const response = await axios.post(url, {
+        username: data.username,
+        password: data.password,
+      });
 
-        console.log("Login successful:", response.data);
+      console.log("Login successful:", response.data);
 
-        if (response.data.user) {  
-            localStorage.setItem('token', response.data.token);
-            console.log("Token stored.");
+      if (response.data.user) {
+        localStorage.setItem("token", response.data.token);
+        console.log("Token stored.");
 
-            console.log("User data received:", response.data.user);
-            localStorage.setItem("user", JSON.stringify(response.data.user)); 
+        console.log("User data received:", response.data.user);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
 
-            navigate('/attendance');
-        } else {
-            console.error("Error: User data is missing in response!");
-        }
-
+        navigate("/attendance");
+      } else {
+        console.error("Error: User data is missing in response!");
+      }
     } catch (error) {
-        console.error("Login error:", error);
-        if (error.response) {
-            setError("root", { message: error.response.data.error });
-        } else {
-            setError("root", { message: "Something went wrong. Please try again." });
-        }
+      console.error("Login error:", error);
+      if (error.response) {
+        setError("root", { message: error.response.data.error });
+      } else {
+        setError("root", {
+          message: "Something went wrong. Please try again.",
+        });
+      }
     }
-};
+  };
 
   return (
     <div>
@@ -88,22 +88,22 @@ const LoginForm = () => {
             <div className="alert alert-danger"> {errors.password.message}</div>
           )}
 
-          <button
-            disabled={isSubmitting}
-            className="btn btn-primary submit-btn login-form-element"
-          >
-            {isSubmitting ? "Loading..." : "Login"}
-          </button>
+          <div className="loginButtonBox">
+            <button
+              disabled={isSubmitting}
+              className="btn btn-primary submit-btn login-form-element"
+            >
+              {isSubmitting ? "Loading..." : "Login"}
+            </button>
 
-          <button 
-          type="button" 
-          className="btn btn-secondary login-form-element"
-          onClick={() => navigate("/change-password")}
-        >
-          Change Your Password
-        </button>
-
-
+            <button
+              type="button"
+              className="btn submit-btn login-form-element"
+              onClick={() => navigate("/change-password")}
+            >
+              Change Your Password
+            </button>
+          </div>
           {errors.root && (
             <div className="alert alert-danger"> {errors.root.message}</div>
           )}
